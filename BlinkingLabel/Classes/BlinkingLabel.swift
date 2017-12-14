e9a04639cfc22bf151f03e7230255cc9010301c8
sarathkumar19 <g.sarathkumarphoenix@gmail.com>
2017-12-14 12:09:37 +0530

    feat: Blinking Label

---
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -0,0 +1,8 @@
+//
+//  BlinkingLabel.swift
+//  BlinkingLabel
+//
+//  Created by Sarath Kumar G on 14/12/17.
+//
+
+import Foundation
--- Example/BlinkingLabel/ViewController.swift
+++ Example/BlinkingLabel/ViewController.swift
@@ -7,18 +7,39 @@
 //
 
 import UIKit
+import BlinkingLabel
 
 class ViewController: UIViewController {
 
+    var isBlinking = false
+    let blinkingLabel = BlinkingLabel(frame: CGRect(x: 150, y: 200, width: 200, height: 30))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
+        
+        // Setup the BlinkingLabel
+        blinkingLabel.text = "I blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+        
+        // Create a UIButton to toggle the blinking
+        let toggleButton = UIButton(frame: CGRect(x: 100, y: 250, width: 150, height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(UIColor.purple, for: .normal)
+        toggleButton.backgroundColor = UIColor.cyan
+        toggleButton.layer.cornerRadius = 7
+        toggleButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        toggleButton.addTarget(self, action: #selector(toggleBlinking), for: .touchUpInside)
+        view.addSubview(toggleButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func toggleBlinking() {
+        (isBlinking) ? blinkingLabel.stopBlinking() : blinkingLabel.startBlinking()
+        isBlinking = !isBlinking
     }
-
 }
 
